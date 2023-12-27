724491d8db29615d1a8f58e6df18dc64c7f0860e
KT <khoatran.yuji@gmail.com>
2023-12-27 15:40:38 +0700

    handing errors

---
+++ app/models/cart.rb
@@ -0,0 +1,29 @@
+class Cart < ApplicationRecord
+  has_many :line_items, dependent: :destroy
+  
+  def add_product(product)
+    current_item = line_items.find_by(product_id: product.id)
+    if current_item
+      curren_item.quantity += 1
+    else
+      current_item = line_items.build(product_id: product_id)
+    end
+    current_item
+  end
+
+  def create
+    product = Product.find(params[:product_id])
+    @line_item = @cart.add_product(product)
+
+    respond_to do |format|
+      if @line_item.save
+        format.html { redicrect_to cart_url(@line_item.cart), notice: "Line item was successfully created." }
+        format.json { render :show, status: :created, location: @line_item }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+end
