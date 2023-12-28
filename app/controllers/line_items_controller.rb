798420f4d92d33e9ebc91d6e5cc412a0ce87248e
KT <khoatran.yuji@gmail.com>
2023-12-28 16:16:34 +0700

    hotwiring the storefront

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,8 +28,9 @@ class LineItemsController < ApplicationController
     # @line_item = LineItem.new(line_item_params)
 
     respond_to do |format|
-      if @line_item.save
-        format.html { redirect_to cart_url(@line_item.cart) }
+      if @line_item.save 
+        format.turbo_stream { @current_item = @line_item }
+        format.html { redirect_to store_index_url }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new, status: :unprocessable_entity }
