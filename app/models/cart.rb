58284ce5c8fe79cc65a01787943ba5a639d8a384
KT <khoatran.yuji@gmail.com>
2024-01-02 10:06:05 +0700

    capturing an order

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,9 +4,9 @@ class Cart < ApplicationRecord
   def add_product(product)
     current_item = line_items.find_by(product_id: product.id)
     if current_item
-      curren_item.quantity += 1
+      current_item.quantity += 1
     else
-      current_item = line_items.build(product_id: product_id)
+      current_item = line_items.build(product_id: product.id)
     end
     current_item
   end
